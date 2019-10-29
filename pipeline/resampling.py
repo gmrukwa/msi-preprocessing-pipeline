166d32316d45da9e7f73aeef075cc79055353e27
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-10-29 10:34:24 +0100

    Refactor resampling task to find the axis

--- pipeline/resampling.py
+++ pipeline/resampling.py
@@ -6,16 +6,10 @@ from pipeline.docker import BasicDockerTask
 
 
 class FindResamplingAxis(BasicDockerTask):
-    intermediate_dir = luigi.Parameter()
-    
     @property
     def command(self):
-        return 'cp /data/LICENSE /data/out'
-    
-    @property
-    def binds(self):
-        return ['%s:/data' % self.intermediate_dir]
+        return 'python -m bin.estimate_resampling_axis /data /intermediate'
 
     def output(self):
         return luigi.LocalTarget(
-            os.path.join(self.intermediate_dir, 'out'))
+            os.path.join(self.intermediate_dir, 'new_mz_axis.txt'))
