65d356e97d844193ab03244faa97fca5f0c42074
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-10-29 10:14:43 +0100

    Create basic docker pipeline

---
+++ pipeline/docker.py
@@ -0,0 +1,51 @@
+import json
+import logging
+
+import docker
+import luigi
+import luigi.contrib.docker_runner
+
+
+logger = logging.getLogger('luigi-interface')
+
+
+class DockerfileTask(luigi.contrib.docker_runner.DockerTask):
+    @property
+    def dockerfile(self):
+        return None
+
+    @property
+    def tag(self):
+        return None
+
+    @property
+    def image(self):
+        return self.tag
+
+    @property
+    def path(self):
+        return '.'
+
+    def run(self):
+        # This addresses the bug in the docker_runner
+        self._image = self.image
+        
+        docker_host = self.docker_url or 'localhost'
+        logger.info('Building image %s from %s on %s',
+                    self._image, self.dockerfile, docker_host)
+        log = self._client.build(
+            path=self.path, dockerfile=self.dockerfile, tag=self.tag)
+        for msg in log:
+            for line in msg.decode().splitlines():
+                logger.debug(json.loads(line).get('stream', ''))
+        super().run()
+
+
+class BasicDockerTask(DockerfileTask):
+    @property
+    def dockerfile(self):
+        return 'docker/basic.dockerfile'
+    
+    @property
+    def tag(self):
+        return 'msi-preprocessing:basic'
