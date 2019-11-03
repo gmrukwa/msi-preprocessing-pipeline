e0bc13265bb4b577a457f499fa1bcdf66288dd74
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-11-03 10:39:45 +0000

    Move to_csv utility

--- to_csv.py
+++ to_csv.py
@@ -1,12 +1,14 @@
 import argparse
 import numpy as np
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--src', help='source .npy file', required=True)
     parser.add_argument('--dst', help='destination .csv file', required=True)
     parser.add_argument('--fmt', help='output file numbers format', default='%.18e')
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
