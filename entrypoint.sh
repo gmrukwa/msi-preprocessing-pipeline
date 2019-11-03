2dda4d89140c18ae37f43f6cdfbae5b6e3bfe149
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-11-03 16:47:11 +0100

    Make entrypoint actually runable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-luigid --background
-luigi --module pipeline PreprocessingPipeline --datasets "$@"
+luigid &>/dev/null &
+PYTHONPATH='.' luigi --module pipeline PreprocessingPipeline --datasets "$@"
+pkill luigid
