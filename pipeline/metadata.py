7290a5723da5c623a57190191ffd50d18169d785
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-11-03 10:33:11 +0000

    Remove metadata extraction dependency on bin

--- pipeline/metadata.py
+++ pipeline/metadata.py
@@ -1,11 +1,48 @@
 from functools import partial
 import os
+import re
 
+from functional import broadcast, for_each, pipe, progress_bar
 import luigi
 import numpy as np
 import pandas as pd
 
+from components.io_utils import text_files
+
 from pipeline._base import *
+
+
+save_csv = partial(pd.DataFrame.to_csv, index=False)
+
+
+metadata_pattern = re.compile('_R[0-9]+X[0-9]+Y[0-9]+_')
+
+
+def component_length(component_with_suffix):
+    for idx, element in enumerate(component_with_suffix):
+        if not element.isdigit():
+            return idx
+    return len(component_with_suffix)
+
+
+def get_component(filename, component):
+    match = metadata_pattern.search(filename)
+    if match is None:
+        raise ValueError(filename)
+    metadata = filename[match.start() + 1:match.end() - 1]
+    metadata = metadata[metadata.find(component) + 1:]
+    metadata = metadata[:component_length(metadata)]
+    return int(metadata)
+
+
+spectrum_metadata = pipe(
+    broadcast(
+        partial(get_component, component='R'),
+        partial(get_component, component='X'),
+        partial(get_component, component='Y')
+    ),
+    np.array
+)
 
 
 class AssembleMetadata(BaseTask):
@@ -18,16 +55,6 @@ class AssembleMetadata(BaseTask):
         return self._as_target("{0}.csv".format(self.dataset))
 
     def run(self):
-        from components.io_utils import text_files
-        from bin.assemble_metadata import (
-            spectrum_metadata,
-            save_csv,
-        )
-        from functional import (
-            for_each,
-            pipe,
-            progress_bar,
-        )
         gather_metadata = pipe(
             text_files, list,
             partial(LuigiTqdm, task=self),
