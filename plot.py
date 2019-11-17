1ec6882da4910205e792d7e1319e550c5b28c55e
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-11-17 19:20:15 +0100

    Fix threshold plotting issues

--- plot.py
+++ plot.py
@@ -4,11 +4,11 @@ from scipy.stats import norm
 
 
 def histogram(vals, thresholds):
-    hist = go.Histogram(x=vals)
-    h_down = hist.ybins.start
-    h_up = hist.ybins.end
+    nbins = int(np.sqrt(vals.size))
+    h_up = np.histogram(vals, bins=nbins)[0].max()
+    hist = go.Histogram(x=vals, nbinsx=nbins)
     fig = go.Figure(data=[hist] + [
-        go.Scatter(x=[x, x], y=[h_down, h_up], mode='lines',
+        go.Scatter(x=[x, x], y=[0, h_up], mode='lines',
                    name='Threshold {0}'.format(idx + 1))
         for idx, x in enumerate(thresholds)
     ])
